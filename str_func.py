986ef3066c4e75006ad99d69718e56ffb1c8159a
EgorZH <egor2001zhukov@gmail.com>
2023-04-17 16:49:58 +0300

    add function str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,13 @@
+'''Хорошая функция'''
 def big_letter(string_: str):
     return string_.upper()
+
+
+'''Функция меняет регистр первой буквы каждого словов строке'''
+
+
+def big_first_letter(string_: str):
+    result = []
+    for word in string_.split(" "):
+        result.append(word.title())
+    return " ".join(result)
